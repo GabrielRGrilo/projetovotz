3d557eb64036c0b393968081c05af463d94020c8
Marco Binda <marcobinda@yahoo.com>
2024-12-02 19:06:16 -0300

    Update protectedRoutes.jsx

--- src/Auth/protectedRoutes.jsx
+++ src/Auth/protectedRoutes.jsx
@@ -2,9 +2,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import Cookies from "js-cookie";
 
 // Função para verificar se o usuário está autenticado
-const isAuthenticated = () => {
+const isAuthenticated = async () => {
   // Verifique se o token está salvo nos cookies
-  const token = Cookies.get("token"); // Use Cookies.get() para pegar o token
+  const token = await Cookies.get("token"); // Use Cookies.get() para pegar o token
   console.log('Token encontrado nos cookies protectedRoutes.jsx:', token);
   return !!token; // Retorna true se o token existir, indicando que o usuário está autenticado
 };
