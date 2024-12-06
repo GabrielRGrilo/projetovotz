cf9f50c368afeb343cb558319fec82e9e2c8aa84
Marco Binda <marcobinda@yahoo.com>
2024-12-06 19:57:21 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,6 @@ import axios from "axios"
 
 
 export const api = axios.create({
-    baseURL: 'https://pmv-ads-2023-2-e4-proj-dad-t2-votz-1.onrender.com/api'
+    baseURL: 'https://pmv-ads-2023-2-e4-proj-dad-t2-votz.onrender.com/api'
+    // baseURL: 'https://pmv-ads-2023-2-e4-proj-dad-t2-votz-1.onrender.com/api'
 })
